43aa4f50e03b6dc14e34380f00a1e39da740f50c
许杰 <496668905@qq.com>
2020-10-30 22:37:26 +0800

    岛屿的周长

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -89,8 +89,104 @@ namespace LeetCode
             //Console.WriteLine(LongestMountain(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
             //Console.WriteLine(SmallerNumbersThanCurrent(new int[] { 8, 1, 2, 2, 3 }));
             //Console.WriteLine(PreorderTraversal(new TreeNode(1, null, new TreeNode(2, new TreeNode(3)))));
-            Console.WriteLine(SumNumbers(new TreeNode(1, new TreeNode(2, new TreeNode(4)), new TreeNode(3))));
+            //Console.WriteLine(SumNumbers(new TreeNode(1, new TreeNode(2, new TreeNode(4)), new TreeNode(3))));
+            Console.WriteLine(IslandPerimeter(new int[][] {
+                new int[] { 0,1,0,0 },
+                new int[] { 1,1,1,0 },
+                new int[] { 0,1,0,0 },
+                new int[] { 1,1,0,0 },
+            }));
             Console.ReadKey();
+        }
+
+
+        static int[] dx = { 0, 1, 0, -1 };
+        static int[] dy = { 1, 0, -1, 0 };
+        /// <summary>
+        /// 岛屿的周长
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <returns></returns>
+        public static int IslandPerimeter(int[][] grid)
+        {
+            //int n = grid.Length, m = grid[0].Length;
+            //int ans = 0;
+            //for (int i = 0; i < n; ++i)
+            //{
+            //    for (int j = 0; j < m; ++j)
+            //    {
+            //        if (grid[i][j] == 1)
+            //        {
+            //            int cnt = 0;
+            //            for (int k = 0; k < 4; ++k)
+            //            {
+            //                int tx = i + dx[k];
+            //                int ty = j + dy[k];
+            //                if (tx < 0 || tx >= n || ty < 0 || ty >= m || grid[tx][ty] == 0)
+            //                {
+            //                    cnt += 1;
+            //                }
+            //            }
+            //            ans += cnt;
+            //        }
+            //    }
+            //}
+            //return ans;
+
+            //int n = grid.Length, m = grid[0].Length;
+            //int ans = 0;
+            //for (int i = 0; i < n; ++i)
+            //{
+            //    for (int j = 0; j < m; ++j)
+            //    {
+            //        if (grid[i][j] == 1)
+            //        {
+            //            ans += DFS(i, j, grid, n, m);
+            //        }
+            //    }
+            //}
+            //return ans;
+
+            int m = grid.Length;
+            int n = grid[0].Length;
+            int ans = 0;
+            for (int i = 0; i < m; ++i)
+                for (int j = 0; j < n; ++j)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        if (i == 0)
+                            ++ans;
+                        else
+                            ans += grid[i - 1][j] == 0 ? 1 : 0;
+                        if (j == 0)
+                            ++ans;
+                        else
+                            ans += grid[i][j - 1] == 0 ? 1 : 0;
+                    }
+                }
+            return ans << 1;
+        }
+
+        public static int DFS(int x, int y, int[][] grid, int n, int m)
+        {
+            if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0)
+            {
+                return 1;
+            }
+            if (grid[x][y] == 2)
+            {
+                return 0;
+            }
+            grid[x][y] = 2;
+            int res = 0;
+            for (int i = 0; i < 4; ++i)
+            {
+                int tx = x + dx[i];
+                int ty = y + dy[i];
+                res += DFS(tx, ty, grid, n, m);
+            }
+            return res;
         }
 
         /// <summary>
