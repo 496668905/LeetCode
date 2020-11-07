e4e2445da2ad30e911284ca4d0b74a7c31960b5d
许杰 <496668905@qq.com>
2020-11-07 22:32:40 +0800

    区间和的个数

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -124,8 +124,93 @@ namespace LeetCode
             //Console.WriteLine(ValidMountainArray(new int[] { 0, 2, 3, 4, 5, 2, 1, 0 }));
             //Console.WriteLine(Insert(new int[][] { new int[] { 1, 3 }, new int[] { 6, 9 } }, new int[] { 2, 5 }));
             //Console.WriteLine(LadderLength("hit", "cog", new List<string> { "hot", "dot", "dog", "lot", "log", "cog" }));
-            Console.WriteLine(SortByBits(new int[] { 2, 3, 5, 7, 11, 13, 17, 19 }));
+            //Console.WriteLine(SortByBits(new int[] { 2, 3, 5, 7, 11, 13, 17, 19 }));
+            Console.WriteLine(CountRangeSum(new int[] { -2, 5, -1 }, -2, 2));
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 区间和的个数
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="lower"></param>
+        /// <param name="upper"></param>
+        /// <returns></returns>
+        public static int CountRangeSum(int[] nums, int lower, int upper)
+        {
+            long s = 0;
+            long[] sum = new long[nums.Length + 1];
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                s += nums[i];
+                sum[i + 1] = s;
+            }
+            return CountRangeSumRecursive(sum, lower, upper, 0, sum.Length - 1);
+        }
+
+        public static int CountRangeSumRecursive(long[] sum, int lower, int upper, int left, int right)
+        {
+            if (left == right)
+            {
+                return 0;
+            }
+            else
+            {
+                int mid = (left + right) / 2;
+                int n1 = CountRangeSumRecursive(sum, lower, upper, left, mid);
+                int n2 = CountRangeSumRecursive(sum, lower, upper, mid + 1, right);
+                int ret = n1 + n2;
+
+                // 首先统计下标对的数量
+                int i = left;
+                int l = mid + 1;
+                int r = mid + 1;
+                while (i <= mid)
+                {
+                    while (l <= right && sum[l] - sum[i] < lower)
+                    {
+                        l++;
+                    }
+                    while (r <= right && sum[r] - sum[i] <= upper)
+                    {
+                        r++;
+                    }
+                    ret += r - l;
+                    i++;
+                }
+
+                // 随后合并两个排序数组
+                int[] sorted = new int[right - left + 1];
+                int p1 = left, p2 = mid + 1;
+                int p = 0;
+                while (p1 <= mid || p2 <= right)
+                {
+                    if (p1 > mid)
+                    {
+                        sorted[p++] = (int)sum[p2++];
+                    }
+                    else if (p2 > right)
+                    {
+                        sorted[p++] = (int)sum[p1++];
+                    }
+                    else
+                    {
+                        if (sum[p1] < sum[p2])
+                        {
+                            sorted[p++] = (int)sum[p1++];
+                        }
+                        else
+                        {
+                            sorted[p++] = (int)sum[p2++];
+                        }
+                    }
+                }
+                for (int j = 0; j < sorted.Length; j++)
+                {
+                    sum[left + j] = sorted[j];
+                }
+                return ret;
+            }
         }
 
         /// <summary>
