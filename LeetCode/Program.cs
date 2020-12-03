14dba2f87671f0784235697706fee5cb20c90cf8
许杰 <496668905@qq.com>
2020-12-03 19:23:15 +0800

    拼接最大数

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -155,6 +155,101 @@ namespace LeetCode
         }
 
         /// <summary>
+        /// 拼接最大数
+        /// </summary>
+        /// <param name="nums1"></param>
+        /// <param name="nums2"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int[] MaxNumber(int[] nums1, int[] nums2, int k)
+        {
+            int m = nums1.Length, n = nums2.Length;
+            int[] maxSubsequence = new int[k];
+            int start = Math.Max(0, k - n), end = Math.Min(k, m);
+            for (int i = start; i <= end; i++)
+            {
+                int[] subsequence1 = MaxSubsequence(nums1, i);
+                int[] subsequence2 = MaxSubsequence(nums2, k - i);
+                int[] curMaxSubsequence = Merge(subsequence1, subsequence2);
+                if (Compare(curMaxSubsequence, 0, maxSubsequence, 0) > 0)
+                {
+                    Array.Copy(curMaxSubsequence, 0, maxSubsequence, 0, k);
+                }
+            }
+            return maxSubsequence;
+        }
+
+        public static int[] MaxSubsequence(int[] nums, int k)
+        {
+            int length = nums.Length;
+            int[] stack = new int[k];
+            int top = -1;
+            int remain = length - k;
+            for (int i = 0; i < length; i++)
+            {
+                int num = nums[i];
+                while (top >= 0 && stack[top] < num && remain > 0)
+                {
+                    top--;
+                    remain--;
+                }
+                if (top < k - 1)
+                {
+                    stack[++top] = num;
+                }
+                else
+                {
+                    remain--;
+                }
+            }
+            return stack;
+        }
+
+        public static int[] Merge(int[] subsequence1, int[] subsequence2)
+        {
+            int x = subsequence1.Length, y = subsequence2.Length;
+            if (x == 0)
+            {
+                return subsequence2;
+            }
+            if (y == 0)
+            {
+                return subsequence1;
+            }
+            int mergeLength = x + y;
+            int[] merged = new int[mergeLength];
+            int index1 = 0, index2 = 0;
+            for (int i = 0; i < mergeLength; i++)
+            {
+                if (Compare(subsequence1, index1, subsequence2, index2) > 0)
+                {
+                    merged[i] = subsequence1[index1++];
+                }
+                else
+                {
+                    merged[i] = subsequence2[index2++];
+                }
+            }
+            return merged;
+        }
+
+        public static int Compare(int[] subsequence1, int index1, int[] subsequence2, int index2)
+        {
+            int x = subsequence1.Length, y = subsequence2.Length;
+            while (index1 < x && index2 < y)
+            {
+                int difference = subsequence1[index1] - subsequence2[index2];
+                if (difference != 0)
+                {
+                    return difference;
+                }
+                index1++;
+                index2++;
+            }
+            return (x - index1) - (y - index2);
+        }
+
+        /// <summary>
         /// 在排序数组中查找元素的第一个和最后一个位置
         /// </summary>
         /// <param name="nums"></param>
